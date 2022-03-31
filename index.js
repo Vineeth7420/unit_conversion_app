79c82bf77f9e7946b971701bc678cbab94a8ba10
Vineeth7420 <vineeth.chimbili@gmail.com>
2022-03-31 08:32:50 +0100

    unit conversion
    covert units from imperial to metric

---
+++ index.js
@@ -0,0 +1,37 @@
+// javascript
+
+let count = 100
+
+const countEl = document.getElementById('count')
+const lengthEl = document.getElementById('length')
+const volumeEl = document.getElementById('volume')
+const massEl = document.getElementById('mass')
+
+
+
+countEl.textContent = count
+
+
+function length(){
+    let meters = count * 3.2
+    let feet =  count / 3.2
+    lengthEl.textContent = `${count} meters = ${meters} feet | ${count} feet = ${feet} meters`
+}
+
+function volume(){
+    let litres = count / 3.78
+    let gallon = count * 3.78
+    volumeEl.textContent = `${count} litres = ${litres.toFixed(3)} gallons | ${count} gallons = ${gallon} litres`
+}
+
+function mass(){
+    let kgs = count * 2.2
+    let pounds = count / 2.2
+    massEl.textContent = `${count} kilos = ${kgs.toFixed(3)} pounds | ${count} pounds = ${pounds.toFixed(3)} kgs`
+}
+
+length()
+volume()
+mass()
+
+
